04dd312b599fdcb7c86874b158f1532b9fa4ae90
tikitiki2 <108548646+tikitiki2@users.noreply.github.com>
2022-11-05 15:06:33 -0400

    final

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 
+
+
+
 #Ethan Yitzhaky
+
 #step 1
 import csv
 dataset=open('Data.csv','r')
-li=list(dataset)
-for i in dataset:
+data=csv.reader(dataset)
+
+for i in data:
     print(i)
-    pass
 
 #step 2
 
@@ -37,9 +41,9 @@ for i in stats:
     years.append(data[0])
     values.append(int(data[1].replace('\n','')))
 plt.bar(years,values)
-plt.title('total sales by year')
+plt.title('total cars sold by year')
 plt.xlabel('years')
-plt.ylabel('total sales amounts')
+plt.ylabel('amounts of cars')
 plt.show()
 
 #step 4
@@ -54,7 +58,8 @@ for i in data:
             num=int(i[j])
             sume.append(num)
         sums.append(sum(sume))
-growthrate=(sums[0]-sums[1])/sums[0]
+
+growthrate=((sums[1]-sums[0])/sums[1])/100
 stats.write(f'growth rate,{growthrate}\n')
 
 dataset.seek(0)
@@ -63,18 +68,16 @@ ans=[]
 for i,li in enumerate(data):
     if i==0:
         months=li[7:13]
-        print(li[7:13])
     elif i==10:
         for j in li[7:13]:
-            sales=int(j)+int(j)*growthrate
+            sales=int(j)+int(j)*-0.017
             ans.append(sales)
-        print(li[7:13])
-stats.write(f'last 6 months,{ans}\n')
+stats.write(f'last 6 months of 2022,{ans}\n')
 
 #step 5
 plt.figure(2)
 plt.barh(months,ans)
-plt.xlabel('sales amount')
+plt.xlabel('cars sold')
 plt.ylabel('months')
 plt.grid()
 plt.show()
